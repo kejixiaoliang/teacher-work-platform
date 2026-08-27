16f405286b682768cb30040953da41c86d81ebbf
kejixiaoliang <1041900576@qq.com>
2026-08-27 18:16:51 +0800

    test: update mini program empty home contract

--- tests/miniprogram-teacher-data.test.js
+++ tests/miniprogram-teacher-data.test.js
@@ -28,5 +28,7 @@ test('index page presents class and student data states', () => {
   assert.match(wxml, /学生/);
   assert.match(wxml, /loading/);
   assert.match(wxml, /error/);
-  assert.match(wxml, /导入 JSON 数据/);
+  assert.match(wxml, /开始建立你的班级实验本/);
+  assert.match(wxml, /导入已有 JSON 数据/);
+  assert.match(wxml, /先看看工作台/);
 });
